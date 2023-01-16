0a2a0b0bf9f9f4e97cfe0f50c8519963406e7ac6
badeai <90259268+badeai@users.noreply.github.com>
2023-01-16 22:04:28 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,5 @@
 import "package:flutter/material.dart";
 
 void main() {
-  return runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: const Text('Hello World'),
-    ),
-    body: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.red,
-          ),
-          child: const Text('One'),
-        ),
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.green,
-          ),
-          child: const Text('Two'),
-        ),
-        Container(
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-          ),
-          child: const Text('Three'),
-        ),
-      ],
-    ),
-  )));
+  return runApp(MaterialApp(home: LocationDetail()));
 }
